4f7a78c4e16ec4bf91614ff138895ef00bb1e6a8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SchedulerCustomAppointmentForm/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+#region #usings
+using DevExpress.XtraScheduler;
+using DevExpress.Xpf.Scheduler;
+#endregion #usings
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        SchedulerTestDataSet dataSet;
+        SchedulerTestDataSetTableAdapters.AppointmentsTableAdapter adapter;
+        SchedulerTestDataSetTableAdapters.ResourcesTableAdapter resourcesAdapter;
+
+        public MainWindow() {
+            InitializeComponent();
+            this.dataSet = new SchedulerTestDataSet();
+
+            // Bind the scheduler storage to appointment data.   
+            this.Scheduler.Storage.AppointmentStorage.DataSource = dataSet.Appointments;
+
+            // Load data to the 'CarsDBDataSet.CarScheduling' table.    
+            this.adapter = new SchedulerTestDataSetTableAdapters.AppointmentsTableAdapter();
+            this.adapter.Fill(dataSet.Appointments);
+
+            // Bind the scheduler storage to resource data.   
+            this.Scheduler.Storage.ResourceStorage.DataSource = dataSet.Resources;
+
+            // Load data to the 'CarsDBDataSet.Cars' table.    
+            this.resourcesAdapter = new SchedulerTestDataSetTableAdapters.ResourcesTableAdapter();
+            resourcesAdapter.Fill(dataSet.Resources);
+
+            this.Scheduler.Storage.AppointmentsInserted +=
+                new PersistentObjectsEventHandler(Storage_AppointmentsModified);
+            this.Scheduler.Storage.AppointmentsChanged +=
+                new PersistentObjectsEventHandler(Storage_AppointmentsModified);
+            this.Scheduler.Storage.AppointmentsDeleted +=
+                new PersistentObjectsEventHandler(Storage_AppointmentsModified);
+          
+        }
+
+       
+
+        void Storage_AppointmentsModified(object sender, PersistentObjectsEventArgs e)
+        {
+            this.adapter.Adapter.Update(this.dataSet);
+            this.dataSet.AcceptChanges();
+
+        }
+    
+
+        #region #editappointmentformshowing
+        private void Scheduler_EditAppointmentFormShowing(object sender, EditAppointmentFormEventArgs e)
+        {
+            e.Form = new CustomAppointmentForm(Scheduler, e.Appointment);
+        }
+        #endregion #editappointmentformshowing
+    }
+}
---
+++ VB/SchedulerCustomAppointmentForm/MainWindow.xaml.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Windows
+#Region "#usings"
+Imports DevExpress.XtraScheduler
+Imports DevExpress.Xpf.Scheduler
+#End Region ' #usings
+
+Namespace WpfApplication1
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Private dataSet As SchedulerTestDataSet
+        Private adapter As SchedulerTestDataSetTableAdapters.AppointmentsTableAdapter
+        Private resourcesAdapter As SchedulerTestDataSetTableAdapters.ResourcesTableAdapter
+
+        Public Sub New()
+            InitializeComponent()
+            Me.dataSet = New SchedulerTestDataSet()
+
+            ' Bind the scheduler storage to appointment data.   
+            Me.Scheduler.Storage.AppointmentStorage.DataSource = dataSet.Appointments
+
+            ' Load data to the 'CarsDBDataSet.CarScheduling' table.    
+            Me.adapter = New SchedulerTestDataSetTableAdapters.AppointmentsTableAdapter()
+            Me.adapter.Fill(dataSet.Appointments)
+
+            ' Bind the scheduler storage to resource data.   
+            Me.Scheduler.Storage.ResourceStorage.DataSource = dataSet.Resources
+
+            ' Load data to the 'CarsDBDataSet.Cars' table.    
+            Me.resourcesAdapter = New SchedulerTestDataSetTableAdapters.ResourcesTableAdapter()
+            resourcesAdapter.Fill(dataSet.Resources)
+
+            AddHandler Scheduler.Storage.AppointmentsInserted, AddressOf Storage_AppointmentsModified
+            AddHandler Scheduler.Storage.AppointmentsChanged, AddressOf Storage_AppointmentsModified
+            AddHandler Scheduler.Storage.AppointmentsDeleted, AddressOf Storage_AppointmentsModified
+
+        End Sub
+
+
+
+        Private Sub Storage_AppointmentsModified(ByVal sender As Object, ByVal e As PersistentObjectsEventArgs)
+            Me.adapter.Adapter.Update(Me.dataSet)
+            Me.dataSet.AcceptChanges()
+
+        End Sub
+
+
+        #Region "#editappointmentformshowing"
+        Private Sub Scheduler_EditAppointmentFormShowing(ByVal sender As Object, ByVal e As EditAppointmentFormEventArgs)
+            e.Form = New CustomAppointmentForm(Scheduler, e.Appointment)
+        End Sub
+        #End Region ' #editappointmentformshowing
+    End Class
+End Namespace
